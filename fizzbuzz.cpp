7b2645858fcbbf597b443924e323e225d2180140
CW47 <cwheeler@udel.edu>
2014-01-12 00:19:38 -0500

    optimized fizzbuzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,3 +1,8 @@
+//FizzBuzz
+//This program outputs numbers from 1 t0 100
+//It prints "Fizz" instead of nums divisible by 3 and "Buzz" for nums divisible by 5.
+//If count is divisible by both 3 and 5 prints "FizzBuzz"
+
 #include <iostream>
 
 int fizzbuzz(int count);
@@ -8,14 +13,18 @@ int main(){
 }
 
 int fizzbuzz(int count){
-	if (count % 3 == 0 && count % 5 == 0)
-		std::cout << "FizzBuzz \n";
-	else if (count % 3 == 0)
-		std::cout << "Fizz \n";
-	else if (count % 5 == 0)
-		std::cout << "Buzz \n";
-	else 
-		std::cout << count << "\n";
+	//divisible by 3?
+	if (count % 3 == 0)
+		std::cout << "Fizz";
+	//divisible by 5?
+	if (count % 5 == 0)
+		std::cout << "Buzz";
+	//divisible by neither?
+	if((count % 3 != 0) && (count % 5 !=0))
+		std::cout << count;
+	//newline after output
+	std::cout << "\n";
+	//stop recursion if count == 100
 	if (count < 100)
 		return fizzbuzz(++count);
 	else return 0;
